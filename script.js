24471ec04e4fa51eb2910241bddce6498f1ad07e
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-01-30 12:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-//your code here!
+let itemCount = 1;
+      const container = document.querySelector(".container");
+      function generateElements(n) {
+        for (let i = 0; i < n; i++) {
+          const element = document.createElement("div");
+          element.innerText = `Item ${itemCount++}`;
+          element.className = "item";
+          container.appendChild(element);
+        }
+      }
+      generateElements(10);
+
+      container.addEventListener("scroll", () => {
+        console.log(container.scrollTop);
+        // maximumScrollableHeight = 500px - 400px = 100px
+        const maximumScrollableHeight =
+          container.scrollHeight - container.clientHeight;
+        const scrolledHeight = container.scrollTop;
+
+        let remainingScrollableHeight =
+          maximumScrollableHeight - scrolledHeight;
+        // threshold to reach  the bottom is 5px
+        if (remainingScrollableHeight <= 5) {
+          generateElements(10);
+        }
+      });
 
